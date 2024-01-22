8a2ded65856327c32711538c84eecfd7a483c009
iMaCHVFX <152030627+iMaCHVFX@users.noreply.github.com>
2024-01-21 20:23:11 -0800

    it work but laggi

--- testinggrounds.gd
+++ testinggrounds.gd
@@ -5,6 +5,9 @@ var sourceStorage = 0
 var atlasCoord : Vector2i
 var prevSource
 var corruptCells = []
+var isArrayInUse : bool
+var arrCounter : int = 0
+var arrCountTemp : int = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,31 +23,48 @@ func _process(delta):
 	sourceStorage = get_cell_source_id(0, cellPos)
 	atlasCoord = get_cell_atlas_coords(0, cellPos)
 	
-	for i in corruptCells:
-		if get_cell_tile_data(0, i).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") < 1:
-			get_cell_tile_data(0, i).set_custom_data("SpreadToPerc", get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") + 0.001)
-		elif get_cell_tile_data(0, i).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") >= 1:
-				if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) != null:
-					get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)).set_custom_data("startCorrupting", true)
-					if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) == -1:
-						corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE))
-				if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)) != null:
-					get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)).set_custom_data("startCorrupting", true)
-					if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)) == -1:
-						corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE))
-				if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) != null:
-					get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)).set_custom_data("startCorrupting", true)
-					if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) == -1:
-						corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE))
-				if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE )) != null:
-					get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE)).set_custom_data("startCorrupting", true)
-					if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE)) == -1:
-						corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE))
-				get_cell_tile_data(0, i).set_custom_data("isCorrupt", true)
-		if get_cell_tile_data(0, i).get_custom_data("isCorrupt") == true and get_cell_source_id(0, i) == 0:
-			set_cell(0, i, 1, get_cell_atlas_coords(0, i))
-		elif get_cell_tile_data(0, i).get_custom_data("isCorrupt") == true and get_cell_source_id(0, i) == 1:
-			pass
+	if arrCountTemp > arrCounter:
+		isArrayInUse = false
+	else:
+		isArrayInUse = true
+	
+	if isArrayInUse:
+		for i in corruptCells:
+			if get_cell_tile_data(0, i).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") < 1:
+				get_cell_tile_data(0, i).set_custom_data("SpreadToPerc", get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") + 0.001)
+			elif get_cell_tile_data(0, i).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") >= 1:
+					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE))
+							arrCounter += 1
+							arrCountTemp += 1
+						
+					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE))
+							arrCounter += 1
+							arrCountTemp += 1
+						
+					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE))
+							arrCounter += 1
+							arrCountTemp += 1
+						
+					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE )) != null:
+						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE))
+							arrCounter += 1
+							arrCountTemp += 1
+						
+					get_cell_tile_data(0, i).set_custom_data("isCorrupt", true)
+			if get_cell_tile_data(0, i).get_custom_data("isCorrupt") == true and get_cell_source_id(0, i) == 0:
+				set_cell(0, i, 1, get_cell_atlas_coords(0, i))
+		
 				
 		
 	
@@ -55,7 +75,10 @@ func _input(event):
 			get_cell_tile_data(0,cellPos).set_custom_data("SpreadToPerc", 100)
 			get_cell_tile_data(0,cellPos).set_custom_data("startCorrupting", true)
 			get_cell_tile_data(0,cellPos).set_custom_data("isCorrupt", true)
+			arrCounter += 1
+			arrCountTemp += 1
+			isArrayInUse = true
 			corruptCells.append(cellPos)
 			
-		print("cell Position: ", cellPos, " sourceStorage: ", sourceStorage, " atlas: ", atlasCoord)
+		#print("cell Position: ", cellPos, " sourceStorage: ", sourceStorage, " atlas: ", atlasCoord)
 				
--- world.gd
+++ world.gd
@@ -1,1 +1,4 @@
 extends Node2D
+
+func _process(delta):
+	print(Engine.get_frames_per_second())
