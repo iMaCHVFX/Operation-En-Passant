527322cd3c6cf4adf6fd853776b391f2f0563bc9
iMaCHVFX <152030627+iMaCHVFX@users.noreply.github.com>
2024-01-21 22:35:10 -0800

    this might break comething

--- testinggrounds.gd
+++ testinggrounds.gd
@@ -1,3 +1,4 @@
+class_name fuckyoufuckyoufuckyoufuckyou
 extends TileMap
 
 var cellPos : Vector2
@@ -5,9 +6,9 @@ var sourceStorage = 0
 var atlasCoord : Vector2i
 var prevSource
 var corruptCells = []
-var isArrayInUse : bool
-var arrCounter : int = 0
-var arrCountTemp : int = 0
+var storageArr = []
+var firstHalf : bool = true
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,49 +24,69 @@ func _process(delta):
 	sourceStorage = get_cell_source_id(0, cellPos)
 	atlasCoord = get_cell_atlas_coords(0, cellPos)
 	
-	if arrCountTemp > arrCounter:
-		isArrayInUse = false
-	else:
-		isArrayInUse = true
 	
-	if isArrayInUse:
-		for i in corruptCells:
-			if get_cell_tile_data(0, i).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") < 1:
-				get_cell_tile_data(0, i).set_custom_data("SpreadToPerc", get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") + 0.001)
-			elif get_cell_tile_data(0, i).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, i).get_custom_data("SpreadToPerc") >= 1:
-					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) != null:
-						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)).set_custom_data("startCorrupting", true)
-						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) == -1:
-							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_RIGHT_SIDE))
-							arrCounter += 1
-							arrCountTemp += 1
+
+	
+	if firstHalf:
+		for i in range(corruptCells.size()/2):
+			var index = i if firstHalf else i + corruptCells.size()/2
+			if get_cell_tile_data(0, corruptCells[index]).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, corruptCells[index]).get_custom_data("SpreadToPerc") < 1:
+				get_cell_tile_data(0, corruptCells[index]).set_custom_data("SpreadToPerc", get_cell_tile_data(0, corruptCells[index]).get_custom_data("SpreadToPerc") + 0.001)
+			elif get_cell_tile_data(0, corruptCells[index]).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, corruptCells[index]).get_custom_data("SpreadToPerc") >= 1:
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE))
 						
-					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)) != null:
-						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)).set_custom_data("startCorrupting", true)
-						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE)) == -1:
-							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_LEFT_SIDE))
-							arrCounter += 1
-							arrCountTemp += 1
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE))
 						
-					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) != null:
-						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)).set_custom_data("startCorrupting", true)
-						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) == -1:
-							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE))
-							arrCounter += 1
-							arrCountTemp += 1
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE))
 						
-					if get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE )) != null:
-						get_cell_tile_data(0, get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE)).set_custom_data("startCorrupting", true)
-						if corruptCells.find(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE)) == -1:
-							corruptCells.append(get_neighbor_cell(i, TileSet.CELL_NEIGHBOR_TOP_SIDE))
-							arrCounter += 1
-							arrCountTemp += 1
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE )) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE))
 						
-					get_cell_tile_data(0, i).set_custom_data("isCorrupt", true)
-			if get_cell_tile_data(0, i).get_custom_data("isCorrupt") == true and get_cell_source_id(0, i) == 0:
-				set_cell(0, i, 1, get_cell_atlas_coords(0, i))
-		
-				
+					get_cell_tile_data(0, corruptCells[index]).set_custom_data("isCorrupt", true)
+			if get_cell_tile_data(0, corruptCells[index]).get_custom_data("isCorrupt") == true and get_cell_source_id(0, corruptCells[index]) == 0:
+				set_cell(0, corruptCells[index], 1, get_cell_atlas_coords(0, corruptCells[index]))
+		firstHalf = false
+	else:
+		for i in range(corruptCells.size()/2):
+			var index = i if firstHalf else i + corruptCells.size()/2
+			if get_cell_tile_data(0, corruptCells[index]).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, corruptCells[index]).get_custom_data("SpreadToPerc") < 1:
+				get_cell_tile_data(0, corruptCells[index]).set_custom_data("SpreadToPerc", get_cell_tile_data(0, corruptCells[index]).get_custom_data("SpreadToPerc") + 0.001)
+			elif get_cell_tile_data(0, corruptCells[index]).get_custom_data("startCorrupting") == true and get_cell_tile_data(0, corruptCells[index]).get_custom_data("SpreadToPerc") >= 1:
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_RIGHT_SIDE))
+						
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_LEFT_SIDE))
+						
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_BOTTOM_SIDE))
+						
+					if get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE )) != null:
+						get_cell_tile_data(0, get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE)).set_custom_data("startCorrupting", true)
+						if corruptCells.find(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE)) == -1:
+							corruptCells.append(get_neighbor_cell(corruptCells[index], TileSet.CELL_NEIGHBOR_TOP_SIDE))
+						
+					get_cell_tile_data(0, corruptCells[index]).set_custom_data("isCorrupt", true)
+			if get_cell_tile_data(0, corruptCells[index]).get_custom_data("isCorrupt") == true and get_cell_source_id(0, corruptCells[index]) == 0:
+				set_cell(0, corruptCells[index], 1, get_cell_atlas_coords(0, corruptCells[index]))	
+		firstHalf = true		
 		
 	
 func _input(event):
@@ -75,10 +96,8 @@ func _input(event):
 			get_cell_tile_data(0,cellPos).set_custom_data("SpreadToPerc", 100)
 			get_cell_tile_data(0,cellPos).set_custom_data("startCorrupting", true)
 			get_cell_tile_data(0,cellPos).set_custom_data("isCorrupt", true)
-			arrCounter += 1
-			arrCountTemp += 1
-			isArrayInUse = true
-			corruptCells.append(cellPos)
+			if corruptCells.find(cellPos) == -1:
+				corruptCells.append(cellPos)
 			
 		#print("cell Position: ", cellPos, " sourceStorage: ", sourceStorage, " atlas: ", atlasCoord)
 				
